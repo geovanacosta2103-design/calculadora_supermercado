353493d3e2e219e863c1eff3f8101773da544c5a
geovana2103 <geovanacosta2103@gmail.com>
2025-09-26 10:48:55 -0300

    Aterado para index.html

---
+++ funcao.js
@@ -0,0 +1,26 @@
+function saudacao() {
+    console.log("Olá, mundo!");
+}
+
+// Chamando a função
+saudacao();
+
+
+ function soma(a,b){
+
+    console.log(3+4)
+ }
+
+ soma(3,4)
+
+ function multiplicacao(r,m){
+    console.log(r*m)
+ }
+
+ multiplicacao(3,4)
+
+ function sayMyName (name){
+    console.log(name)
+ }
+
+sayMyName("Vinicius")
